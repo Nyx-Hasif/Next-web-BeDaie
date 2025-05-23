0c7c7ffc7c3208bd1e5625c6ed55e30746048860
Nyx-Hasif <nikaresif97@gmail.com>
2025-05-24 05:27:00 +0800

    fixed spelling  content

--- components/Interactive.tsx
+++ components/Interactive.tsx
@@ -8,7 +8,7 @@ const Interactive = () => {
 
       <div className=' text-center flex flex-col gap-5 w-[80%] items-center page1-content'>
         <h1 className='sm:text-7xl text-4xl font-bold'>Interaktif Modul</h1>
-        <p className='sm:text-3xl text-xl font-medium'>Suasana pembelajaran yang konduksif</p>
+        <p className='sm:text-3xl text-xl font-medium'>Suasana pembelajaran yang kondusif</p>
         <div className='border-5 border-black'><Image src={assets.interaktif} alt='ekosistem' width={850} height={850} /></div>
       </div>
       
